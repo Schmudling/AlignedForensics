4090d805ec8d1c0db2d0dc662162ac5e3e3a85f7
AniSundar18 <128100639+AniSundar18@users.noreply.github.com>
2024-11-03 19:17:31 -0600

    Update run.sh

--- training_code/recon/run.sh
+++ training_code/recon/run.sh
@@ -1,9 +1,6 @@
-IMAGES_PATH="/nobackup3/anirudh/datasets/coco/train/0_real"
-SHADERS_TRAIN="/nobackup2/anirudh/datasets/Shaders21k/shaders_inv_png/train/0_real"
+#IMAGES_PATH= "ENTER PATH WHERE REAL IMAGES ARE STORED"
 
-#CUDA_VISIBLE_DEVICES=0 python execute.py --input_folder=$IMAGES_PATH --batch_size 32 --repo_id "playgroundai/playground-v2.5-1024px-aesthetic" 
+#CODE TO CREATE THE DATASET
+CUDA_VISIBLE_DEVICES=0 python execute.py --input_folder=$IMAGES_PATH --batch_size 64 --repo_id 'CompVis/ldm-text2im-large-256' 
 
-CUDA_VISIBLE_DEVICES=0 python execute.py --input_folder=$IMAGES_PATH --batch_size 1 --repo_id 'stabilityai/stable-diffusion-3-medium-diffusers' 
-#CUDA_VISIBLE_DEVICES=0 python execute.py --input_folder=$SHADERS_TRAIN --batch_size 64 --repo_id 'runwayml/stable-diffusion-v1-5' 
 
-#--use_ddim --steps=100
