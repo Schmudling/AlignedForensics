99a974061e61ec4dd96a255a2e24a063fc919131
Anirudh <asundararaj2@wisc.edu>
2024-11-10 14:52:51 -0600

    updated training script

--- training_code/script_train.sh
+++ training_code/script_train.sh
@@ -1,13 +1,6 @@
-DATASET="/nobackup/anirudh/datasets/corvi_ldm_inv"
-SHADERS_21K="/nobackup/anirudh/datasets/Shaders21k/shaders_inv"
+#DATASET=path to dataset
 
-
-
-LDM_NAME="ldm/corvi_ldm_flex/"
-LDM_DS_NAME="s10_datasize_ablations_s17/corvi_inv_1k_sync"
-
-
-
+#LDM_DS_NAME=directory of checkpoints
 
 # Train on 1000 real and 1000 fake in the sync mode (use half the usual batch size as the code picks a fake image for every real image)
 CUDA_VISIBLE_DEVICES=2 python train.py --name=$LDM_DS_NAME --arch res50nodown --cropSize 96  --norm_type resnet --resize_size 256 --resize_ratio 0.75 --blur_sig 0.0,3.0 --cmp_method cv2,pil --cmp_qual 30,100 --resize_prob 0.2 --jitter_prob  0.8 --colordist_prob 0.2 --cutout_prob 0.2 --noise_prob 0.2 --blur_prob 0.5 --cmp_prob 0.5 --rot90_prob 1.0 --dataroot $DATASET --batch_size 32 --earlystop_epoch 10 --use_inversions --seed 17 --batched_syncing --data_cap=1000
