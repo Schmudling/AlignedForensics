8203040c8cdf7e9636376a52b507f7d8f5d7b327
AniSundar18 <128100639+AniSundar18@users.noreply.github.com>
2024-11-10 16:41:42 -0600

    Update sweep.sh

--- test_code/sweep.sh
+++ test_code/sweep.sh
@@ -1,6 +1,6 @@
-MID_REAL='/nobackup3/anirudh/datasets/redcaps/samples'
-MID_FAKE='/nobackup3/anirudh/datasets/SDv2-1/cifar-100/res-512/guidance-7-5/images/1_fake'
-DIR='sweep_plots/res/'
+#MID_REAL= real path
+#MID_FAKE= fake path
+#DIR= plots path
 
 if [ -d "$DIR" ]; then
   echo "Directory $DIR already exists."
@@ -11,6 +11,6 @@ else
 fi
 
 
-python resize_sweep.py --real=$MID_REAL --fake=$MID_FAKE --start=128 --end=1024 --stride=250 --device 'cuda:2' --out_path=$DIR --base_res=512
+python resize_sweep.py --real=$MID_REAL --fake=$MID_FAKE --start=128 --end=1024 --stride=10 --device 'cuda:2' --out_path=$DIR --base_res=512
 
-#python resize_sweep.py --real=$MID_REAL --fake=$MID_FAKE --setting 'qual' --start=0 --end=100 --stride=25 --device 'cuda:5' --out_path=$DIR #--base_res=512
+#python resize_sweep.py --real=$MID_REAL --fake=$MID_FAKE --setting 'qual' --start=0 --end=2 --stride=25 --device 'cuda:5' --out_path=$DIR #--base_res=512
